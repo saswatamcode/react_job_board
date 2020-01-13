5efdb5488052ff92b592a92f951e1bdc183001e0
Saswata Mukherjee <51132453+saswatamcode@users.noreply.github.com>
2020-01-13 22:11:51 +0530

    Update index.js

--- worker/index.js
+++ worker/index.js
@@ -2,4 +2,4 @@ var CronJob = require('cron').CronJob;
 
 const fetchGithub = require('./tasks/fetch-github');
 
-new CronJob('* */1 * * *', fetchGithub, null, true, 'America/Los_Angeles');
+new CronJob('* * * * *', fetchGithub, null, true, 'America/Los_Angeles');
