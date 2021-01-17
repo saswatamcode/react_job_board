36ff638d33f168d31efe128b0bd2cb012ea0d74f
Saswata Mukherjee <saswataminsta@yahoo.com>
2021-01-17 10:25:04 +0530

    Restructure project
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,23 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Jobs from './Jobs';
-import Sidebar from './Sidebar';
-
-
-
-const JOB_API_URL = "http://localhost:3001/jobs"
-
-async function fetchJobs(updateCb)
-{
-  const res = await fetch(JOB_API_URL)
-  const json = await res.json()
-  updateCb(json)
-}
 
 function App() {
-  const [jobList, updateJobs] = React.useState([]); 
-  React.useEffect(() =>
-  {
-    fetchJobs(updateJobs);
-  }, [])
   return (
     <div className="App">
-      <Sidebar />
-      <Jobs jobs={jobList}/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
